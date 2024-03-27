3371c13b81f889ee06f45a3b3930bd833b5b66d3
Gabriel-Aranda1406 <biel.aranda234@gmail.com>
2024-03-27 12:54:14 -0300

    add ContaPoupanca

--- Menu.ts
+++ Menu.ts
@@ -1,45 +1,43 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
-import { Conta } from "./src/model/Conta";
+import { Conta } from './src/model/Conta';
+import { ContaCorrente } from './src/model/ContaCorrente';
+import { ContaPoupanca } from './src/model/ContaPoupanca';
 
 export function main() {
 
     let opcao: number;
-
     
-    // Instanciando um novo Objeto da Classe Conta
-    console.log("\nCriar o Objeto da Classe Conta")
-    let c1: Conta = new Conta(1, 123, 1, "Natasha", 1000000);
+   // Objeto da Classe Conta (Teste)
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+    conta.sacar(10500);
+    conta.visualizar();
+    conta.depositar(5000);
+    conta.visualizar();
 
-    // Visualizando todos os dados da conta criada
-    c1.visualizar();
+   // Objeto da Classe ContaCorrente (Teste)
+    const contacorrente: ContaCorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
+    contacorrente.visualizar();
+    contacorrente.sacar(2000);
+    contacorrente.visualizar();
+    contacorrente.depositar(1000);
+    contacorrente.visualizar();
 
-    // Modificando o Saldo através do método set
-    console.log("\nAlterar o Saldo para R$ 1500000.00")
-    c1.set_saldo(1500000);
-
-    // Recuperando o valor do Saldo através do método get
-    console.log(`\nNovo Saldo da Conta: ${c1.get_saldo()}`);
-
-    // Verificando se o Saque deu certo
-    console.log(`\nSacar R$ 2000000.00 da conta: ${c1.sacar(2000000)}`);
-    c1.visualizar();
-
-    // Verificando se o Saque deu certo
-    console.log(`\nSacar R$ 2000.00 da conta: ${c1.sacar(2000)}`);
-    c1.visualizar();
-
-    // Depositar dinheiro na conta
-    console.log("\nDepositar R$ 5000.00 na Conta: ");
-    c1.depositar(5000);
-    c1.visualizar();
+    // Objeto da Classe ContaPoupanca (teste)
+    const contapoupanca: ContaPoupanca = new ContaPoupanca(3, 123, 2, "Victor", 1000, 10);
+    contapoupanca.visualizar();
+    contapoupanca.sacar(200);
+    contapoupanca.visualizar();
+    contapoupanca.depositar(1000);
+    contapoupanca.visualizar();
 
     while (true) {
 
-        console.log(colors.fg.blue, 
+        console.log(colors.bg.black, colors.fg.yellow, 
                     "*****************************************************");
         console.log("                                                     ");
-        console.log("       PagPapo - Pague enquanto troca um papo        ");
+        console.log("      PagPapo - Pague enquanto troca um papo         ");
         console.log("                                                     ");
         console.log("*****************************************************");
         console.log("                                                     ");
@@ -54,17 +52,13 @@ export function main() {
         console.log("            9 - Sair                                 ");
         console.log("                                                     ");
         console.log("*****************************************************");
-        console.log("                                                     ", 
-        colors.reset);
+        console.log("                                                     ", colors.reset);
 
-        console.log(colors.fg.bluestrong,
-            "Entre com a opção desejada: ");
-            console.log(colors.reset, "");
+        console.log("Entre com a opção desejada: ");
         opcao = readlinesync.questionInt("");
 
         if (opcao == 9) {
-            console.log(colors.fg.bluestrong, 
-                "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+            console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
             sobre();
             console.log(colors.reset, "");
             process.exit(0);
@@ -72,56 +66,47 @@ export function main() {
 
         switch (opcao) {
             case 1:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nCriar Conta\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nCriar Conta\n\n", colors.reset);
                 
                 keyPress()
                 break;
             case 2:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nListar todas as Contas\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nListar todas as Contas\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 3:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 4:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nAtualizar dados da Conta\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 5:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nApagar uma Conta\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 6:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nSaque\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 7:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nDepósito\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
 
                 keyPress()
                 break;
             case 8:
-                console.log(colors.fg.bluestrong, 
-                    "\n\nTransferência entre Contas\n\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
 
                 keyPress()
                 break;
             default:
-                console.log(colors.fg.bluestrong, 
-                    "\nOpção Inválida!\n", colors.reset);
+                console.log(colors.fg.whitestrong, "\nOpção Inválida!\n", colors.reset);
 
                 keyPress()
                 break;
@@ -130,11 +115,11 @@ export function main() {
 
 }
 
-
+/* Função com os dados da pessoa desenvolvedora */
 function sobre(): void {
     console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: Gabriel Aranda");
-    console.log("Veja meus outros projetos:");
+    console.log("Projeto Desenvolvido por: Gabriel Aranda ");
+    console.log("Entre e veja outros projetos: ");
     console.log("github.com/Gabriel-Aranda1406");
     console.log("*****************************************************");
 }
